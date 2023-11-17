7fee1bd8d0a970acab3af341bc708a347aaa2d97
Elysia Smyers <elysiaqs@gmail.com>
2023-11-16 18:09:41 -0800

    tiny changes

--- pages/Communities.js
+++ pages/Communities.js
@@ -6,7 +6,11 @@ export default function Communities() {
   return (
     <View style={enosiStyles.feedContainer}>
       <TextInput placeholder="Search" style={enosiStyles.searchBar}></TextInput>
-      <ScrollView style={{ width: "90%" }}>
+      <ScrollView style={{ width: "90%" }} showsVerticalScrollIndicator={false}>
+        <Community
+          name="LGHS Class of 2019"
+          icon={require("../assets/community.png")}
+        ></Community>
         <Community
           name="LGHS Class of 2019"
           icon={require("../assets/community.png")}
