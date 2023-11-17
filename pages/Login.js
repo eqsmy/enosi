6f57d5acf5538c5a9bd6bd0430eb59bd6a000604
Lyndsea Warkenthien <lwarkenthien24@gmail.com>
2023-11-17 09:55:01 -0800

    supabase connnection started

--- pages/Login.js
+++ pages/Login.js
@@ -1,22 +1,69 @@
+import React, { useState } from "react";
 import {
   Image,
-  View,
   TextInput,
-  Text,
-  Button,
   SafeAreaView,
+  StyleSheet,
+  Alert,
+  View,
+  Text,
+  TouchableOpacity,
 } from "react-native";
-import { StyleSheet } from "react-native";
-import { useState } from "react";
+import { supabase } from "../utils/supabase";
 import { BasicButton } from "../components/Buttons";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Login({ setLoggedIn }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [isLogin, setIsLogin] = useState(true);
+  const navigation = useNavigation();
 
-  function login() {
-    setLoggedIn(true);
+  //a function to allow the user to sign in with their email and password
+  async function signInWithEmail() {
+    console.log("sign in with email attempt");
+    try {
+      setLoading(true);
+      const { user, error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+      if (error) throw error;
+      console.log("Sign in response: ", { user, error });
+      if (user) setLoggedIn(true) && navigation.navigate("Home");
+    } catch (error) {
+      Alert.alert(error.message);
+    } finally {
+      setLoading(false);
+    }
   }
+
+  //a function to allow the user to sign up with their email and password
+  async function signUpWithEmail() {
+    try {
+      setLoading(true);
+      const {
+        data: { user },
+        error,
+      } = await supabase.auth.signUp({ email, password });
+
+      if (error) throw error;
+      if (user) {
+        Alert.alert("Check your email for the confirmation link.");
+        setLoggedIn(true) && navigation.navigate("Home"); // Set loggedIn to true on successful sign-up
+      }
+    } catch (error) {
+      Alert.alert(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  //a variable to toggle between sign in and sign up
+  const toggleLoginSignup = () => {
+    setIsLogin(!isLogin);
+  };
 
   return (
     <SafeAreaView style={loginStyles.container}>
@@ -33,17 +80,39 @@ export default function Login({ setLoggedIn }) {
       />
       <TextInput
         style={loginStyles.input}
-        placeholder={"Email address"}
-        value={email}
         onChangeText={setEmail}
+        value={email}
+        placeholder={"email@address.com"}
+        autoCapitalize={"none"}
       ></TextInput>
       <TextInput
         style={loginStyles.input}
-        placeholder={"Password"}
-        value={password}
         onChangeText={setPassword}
+        value={password}
+        secureTextEntry={true}
+        placeholder={"Password"}
+        autoCapitalize={"none"}
       ></TextInput>
-      <BasicButton onPress={login}></BasicButton>
+      {isLogin ? (
+        <BasicButton
+          text="Sign In"
+          onPress={() => signInWithEmail()}
+          loading={loading}
+        />
+      ) : (
+        <BasicButton
+          text="Sign Up"
+          onPress={() => signUpWithEmail()}
+          loading={loading}
+        />
+      )}
+      <TouchableOpacity onPress={toggleLoginSignup}>
+        <Text>
+          {isLogin
+            ? "Don't have an account? Sign Up"
+            : "Already have an account? Sign In"}
+        </Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
