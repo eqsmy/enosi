cdeb8c5cceccdc0da064581f85695796b2aee509
Elysia Smyers <34151362+eqsmy@users.noreply.github.com>
2023-12-02 14:35:23 -0800

    a bunch of stuff (#39)

--- pages/LogActivity.js
+++ pages/LogActivity.js
@@ -12,57 +12,30 @@ import { useNavigation } from "@react-navigation/native";
 import { useState } from "react";
 import { Dropdown } from "react-native-element-dropdown";
 import {
+  FlatList,
   GestureHandlerRootView,
   TouchableOpacity,
 } from "react-native-gesture-handler";
 
-import LogActivity2 from "./LogActivity2";
 import { supabase } from "../utils/Supabase";
 import { useUser } from "../utils/UserContext";
 
 const width = Dimensions.get("window").width;
 const height = Dimensions.get("window").height;
 
-const initialBgColors = {
-  meditate: "transparent",
-  hike: "transparent",
-  swim: "transparent",
-  bike: "transparent",
-  run: "transparent",
-  lift: "transparent",
-};
-
-export default function LogActivity() {
+export function LogActivity1() {
   //utilzie the user context to get user details and pass to activityData
   const { state } = useUser();
   const userId = state.session.user.id ? state.session.user.id : null;
-  //console.log("userId", userId);
-
-  const [bgColors, setBgColors] = useState(initialBgColors);
-  const [activity, setActivity] = useState(null);
+  const [activity, setActivity] = useState("");
   const [input, setInput] = useState("");
   const [inputNum, setNum] = useState("0.0");
   const [val, setValue] = useState(null);
 
   const [textColor, setTextColor] = useState("#C9C9C9");
 
-  const toggleBgColor = (activity) => {
-    setBgColors((prevColors) => ({
-      ...prevColors,
-      [activity]:
-        prevColors[activity] === "transparent" ? "#61B8C2" : "transparent",
-    }));
-  };
-
   //function to handle the sumibssion of the activity data
   const handleSubmit = async () => {
-    // Basic form validation
-    // if (!activity || !inputNum || !val) {
-    //   Alert.alert("Error", "Please fill in all the fields.");
-    //   return;
-    // }
-
-    // Ensure inputNum is a valid number
     const distance = parseFloat(inputNum);
     if (isNaN(distance)) {
       Alert.alert("Error", "Please enter a valid number for distance.");
@@ -110,173 +83,137 @@ export default function LogActivity() {
     console.log("Text clicked!");
   };
 
+  const renderActivityType = ({ item }) => (
+    <View style={{ marginBottom: 10, justifyContent: "space-around", flex: 1 }}>
+      <Text style={styles.boxText}>{item.type}</Text>
+      <TouchableOpacity
+        style={[
+          styles.box,
+          {
+            backgroundColor: item.type == activity ? "#61B8C2" : "transparent",
+          },
+        ]}
+        onPress={() => {
+          setActivity(item.type);
+        }}
+      >
+        <Image style={styles.imageBox} source={item.image} />
+      </TouchableOpacity>
+    </View>
+  );
+
+  const presetActivities = [
+    {
+      type: "Meditate",
+      image: require("../assets/activityIcons/meditate.png"),
+    },
+    { type: "Hike", image: require("../assets/activityIcons/hike.png") },
+    { type: "Swim", image: require("../assets/activityIcons/swim.png") },
+    { type: "Bike", image: require("../assets/activityIcons/bike.png") },
+    { type: "Run", image: require("../assets/activityIcons/run.png") },
+    { type: "Lift", image: require("../assets/activityIcons/lift.png") },
+  ];
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <View>
-        <View>
+    <GestureHandlerRootView style={{ flex: 1, backgroundColor: "white" }}>
+      <View
+        style={{
+          marginHorizontal: "5%",
+          width: "90%",
+        }}
+      >
+        <View style={styles.container}>
+          <FlatList
+            data={presetActivities}
+            numColumns={3}
+            horizontal={false}
+            renderItem={renderActivityType}
+          ></FlatList>
+        </View>
+
+        <Text style={styles.customBoxText}>Custom Activity</Text>
+        <View style={styles.customBox}>
+          <View style={{ flex: 1, justifyContent: "center", paddingLeft: 15 }}>
+            <TextInput
+              placeholder="My Activity"
+              placeholderStyle={styles.textInputStyle}
+              onChangeText={updateActivity}
+              value={input}
+            />
+          </View>
+        </View>
+
+        <View style={styles.numBox}>
+          <View>
+            <TextInput
+              style={{
+                fontSize: 72,
+                color: textColor,
+                textAlign: "right",
+              }}
+              value={inputNum}
+              onChangeText={(x) => {
+                setNum(x);
+                setTextColor("black");
+              }}
+              keyboardType="numeric"
+            />
+          </View>
+        </View>
+
+        <View style={styles.customBox}>
           <View
             style={{
-              marginTop: height * 0.025,
-              marginLeft: width * 0.07,
-              marginRight: width * 0.05,
-              width: width * 0.85,
+              flex: 1,
+              justifyContent: "center",
+              paddingLeft: 15,
+              paddingRight: 15,
             }}
           >
-            <View style={styles.container}>
-              <TouchableOpacity
-                style={[styles.box, { backgroundColor: bgColors.meditate }]}
-                onPress={() => toggleBgColor("meditate")}
-              >
-                <Text style={styles.boxText}>Meditate</Text>
-                <Image
-                  style={styles.imageBox}
-                  source={require("../assets/yoga.png")}
-                />
-              </TouchableOpacity>
+            <Dropdown
+              style={styles.dropdown}
+              data={data}
+              placeholder="Select Units"
+              placeholderStyle={{
+                color: "#c3c3c5",
+                fontWeight: "400",
+                fontFamily: "Arial",
+                fontSize: 15,
+              }}
+              labelField="label"
+              valueField="value"
+              onChange={(item) => {
+                setValue(item.value); // Directly update the 'val' state with the selected item's value
+              }}
+            />
+          </View>
+        </View>
 
-              <TouchableOpacity
-                style={[styles.box, { backgroundColor: bgColors.hike }]}
-                onPress={() => toggleBgColor("hike")}
-              >
-                <Text style={styles.boxText}>Hike</Text>
-                <Image
-                  style={styles.imageBox}
-                  source={require("../assets/hike.png")}
-                />
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={[styles.box, { backgroundColor: bgColors.swim }]}
-                onPress={() => toggleBgColor("swim")}
-              >
-                <Text style={styles.boxText}>Swim</Text>
-                <Image
-                  style={[styles.imageBox, { marginTop: 20 }]}
-                  source={require("../assets/swim.png")}
-                />
-              </TouchableOpacity>
-            </View>
-
-            <View style={styles.container}>
-              <TouchableOpacity
-                style={[styles.box, { backgroundColor: bgColors.bike }]}
-                onPress={() => toggleBgColor("bike")}
-              >
-                <Text style={styles.boxText}>Bike</Text>
-                <Image
-                  style={[styles.imageBox, { marginTop: 15 }]}
-                  source={require("../assets/bike.png")}
-                />
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={[styles.box, { backgroundColor: bgColors.run }]}
-                onPress={() => toggleBgColor("run")}
-              >
-                <Text style={styles.boxText}>Run</Text>
-                <Image
-                  style={styles.imageBox}
-                  source={require("../assets/run.png")}
-                />
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={[styles.box, { backgroundColor: bgColors.lift }]}
-                onPress={() => toggleBgColor("lift")}
-              >
-                <Text style={styles.boxText}>Lift</Text>
-                <Image
-                  style={styles.imageBox}
-                  source={require("../assets/lift.png")}
-                />
-              </TouchableOpacity>
-            </View>
-
-            <Text style={styles.customBoxText}>Custom Activity</Text>
-            <View style={styles.customBox}>
-              <View
-                style={{ flex: 1, justifyContent: "center", paddingLeft: 15 }}
-              >
-                <TextInput
-                  placeholder="My Activity"
-                  placeholderStyle={styles.textInputStyle}
-                  onChangeText={updateActivity}
-                  value={input}
-                />
-              </View>
-            </View>
-
-            <View style={styles.numBox}>
-              <View>
-                <TextInput
-                  style={{
-                    fontSize: 72,
-                    color: textColor,
-                    textAlign: "right",
-                  }}
-                  value={inputNum}
-                  onChangeText={(x) => {
-                    setNum(x);
-                    setTextColor("black");
-                  }}
-                  keyboardType="numeric"
-                />
-              </View>
-            </View>
-
-            <View style={styles.customBox}>
-              <View
+        <View
+          style={{
+            alignItems: "center",
+            justifyContent: "center",
+            flex: 1,
+            marginTop: 45,
+          }}
+        >
+          <View
+            style={[
+              styles.log,
+              { alignItems: "center", justifyContent: "center" },
+            ]}
+          >
+            <TouchableOpacity onPress={handleSubmit}>
+              <Text
                 style={{
-                  flex: 1,
-                  justifyContent: "center",
-                  paddingLeft: 15,
-                  paddingRight: 15,
+                  color: "white",
+                  fontFamily: "Avenir",
+                  fontWeight: "800",
+                  fontSize: 18,
                 }}
               >
-                <Dropdown
-                  style={styles.dropdown}
-                  data={data}
-                  placeholder="Select Units"
-                  placeholderStyle={{
-                    color: "#c3c3c5",
-                    fontWeight: "400",
-                    fontFamily: "Arial",
-                    fontSize: 15,
-                  }}
-                  labelField="label"
-                  valueField="value"
-                  onChange={(item) => {
-                    setValue(item.value); // Directly update the 'val' state with the selected item's value
-                  }}
-                />
-              </View>
-            </View>
-
-            <View
-              style={{
-                alignItems: "center",
-                justifyContent: "center",
-                flex: 1,
-                marginTop: 45,
-              }}
-            >
-              <View
-                style={[
-                  styles.log,
-                  { alignItems: "center", justifyContent: "center" },
-                ]}
-              >
-                <TouchableOpacity onPress={handleSubmit}>
-                  <Text
-                    style={{
-                      color: "white",
-                      fontFamily: "Avenir",
-                      fontWeight: "800",
-                      fontSize: 18,
-                    }}
-                  >
-                    Log
-                  </Text>
-                </TouchableOpacity>
-              </View>
-            </View>
+                Log
+              </Text>
+            </TouchableOpacity>
           </View>
         </View>
       </View>
@@ -284,24 +221,34 @@ export default function LogActivity() {
   );
 }
 
+export function LogActivity2() {
+  return (
+    <SafeAreaView style={enosiStyles.container}>
+      <Text>Challenges</Text>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
     justifyContent: "space-between",
-    marginTop: height * 0.05,
+    marginTop: "5%",
   },
   dropdown: {
     color: "#61B8C2",
     fontWeight: "500",
   },
   box: {
-    width: width * 0.22,
-    height: height * 0.1,
+    height: height * 0.11,
     borderRadius: 15,
     borderWidth: 5,
     borderColor: "#61B8C2",
     marginHorizontal: 10,
     alignItems: "center",
+    justifyContent: "center",
+    marginTop: 30,
   },
   boxText: {
     position: "absolute",
@@ -309,7 +256,7 @@ const styles = StyleSheet.create({
     fontFamily: "Avenir",
     fontWeight: "600",
     fontSize: 18,
-    top: -35,
+    top: 0,
     left: 0,
     right: 0,
   },
@@ -337,8 +284,7 @@ const styles = StyleSheet.create({
   },
   imageBox: {
     justifyContent: "center",
-    marginLeft: -5,
-    marginTop: 10,
+    margin: "auto",
     resizeMode: "cover",
   },
   numStyle: {
@@ -351,7 +297,7 @@ const styles = StyleSheet.create({
     paddingRight: 15,
     height: height / 8,
     maxWidth: "70%",
-    backgroundColor: "#EBEBEB",
+    backgroundColor: "#f6f6f6",
     borderRadius: 20,
     flexDirection: "row",
     justifyContent: "flex-end",
